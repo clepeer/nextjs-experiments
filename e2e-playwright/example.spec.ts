39de7400f598a459e059deb82c054d90a9ebcc87
Milan <miland@ketryx.com>
2023-11-22 08:42:34 +0100

    rm code that was used for debugging

--- e2e-playwright/example.spec.ts
+++ e2e-playwright/example.spec.ts
@@ -1,16 +1,8 @@
 import { test, expect } from './fixtures/fixtures';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://github.com/dzena/github-search-ddd');
-  await page.getByRole('link', { name: 'Issues' }).click();
-  await page.waitForLoadState('networkidle');
+  // user is already authenticated
 
-  await page.getByRole('link', { name: 'New issue' }).click();
-  await page.waitForLoadState('networkidle');
-  await page.fill('input[name="issue[title]"]', 'Test issue');
-  await page.fill('textarea[name="issue[body]"]', 'Test issue body');
-  await page.getByRole('button', { name: 'Submit new issue' }).click();
-  await page.waitForLoadState('networkidle');
-  await page.getByRole('link', { name: 'Issues' }).click();
+  await page.goto('https://github.com/dzena/github-search-ddd');
 });
 
