1f89e9dc783cbeffa9e73cf31db0ddc3a76cc530
Patrick Ecker <ryyppy@users.noreply.github.com>
2024-07-03 12:53:13 +0200

    Fix warning in graphql resolvers

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,7 +1,9 @@
-export default {
+const resolvers = {
   Query: {
-    greetings(_arg: any, _a: any, context: any) {
+    greetings(_arg: any, _a: any, _context: any) {
       return "Hello world.";
     },
   },
-};
+}
+
+export default resolvers;
