5e6ca49e8bfe48a6b8e77c178ab3a8d7fe191093
Patrick Ecker <ryyppy@users.noreply.github.com>
2024-07-03 12:52:51 +0200

    Fix e2e.spec.ts file

--- e2e-playwright/e2e.spec.ts
+++ e2e-playwright/e2e.spec.ts
@@ -1,3 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from './fixtures/fixtures';
+
+test('fix me please', async ({ page }) => {
+  // user is already authenticated
+
+  await page.goto('https://github.com/dzena/github-search-ddd');
+  await page.getByRole('link', { name: 'Issues' }).click();
+
+  await page.getByRole('link', { name: 'New issue' }).click();
+  await page.fill('input[name="issue[title]"]', 'Test issue');
+  await page.fill('textarea[name="issue[body]"]', 'Test issue body');
+  await page.getByRole('button', { name: 'Submit new issue' }).click();
+  await page.getByRole('link', { name: 'Issues' }).click();
+});
 
 test('my testcase', async ({ page }) => {});
